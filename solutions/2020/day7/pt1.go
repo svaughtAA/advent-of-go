ea4ee07b52abca4d0a0c237df45e9256d248eb43
Steven Vaught <steven.vaught@aa.com>
2025-12-01 21:13:49 -0700

    2020 - day 7

---
+++ solutions/2020/day7/pt1.go
@@ -0,0 +1,73 @@
+package day7
+
+import (
+	"advent-of-go/utils"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Pt1() utils.Solution {
+	return utils.Solution{
+		Year:       2020,
+		Day:        7,
+		Part:       1,
+		Calculator: pt1,
+	}
+}
+
+func ParseInput(input string) map[string]([]string) {
+	parentRegex := regexp.MustCompile(`^(?P<parent>.*?) bags contain (?P<rest>.*?)$`)
+	childRegex := regexp.MustCompile(`.*?(?P<amt>[0-9]) (?P<child>.*?) bag?(?P<rest>.*?)$`)
+	results := make(map[string]([]string))
+	for rule := range strings.SplitSeq(input, "\n") {
+		matches := parentRegex.FindStringSubmatch(rule)
+		parent := matches[parentRegex.SubexpIndex("parent")]
+		rest := matches[parentRegex.SubexpIndex("rest")]
+		results[parent] = []string{}
+		for childRegex.MatchString(rest) {
+			matches := childRegex.FindStringSubmatch(rest)
+			amt, _ := strconv.Atoi(matches[childRegex.SubexpIndex("amt")])
+			child := matches[childRegex.SubexpIndex("child")]
+			rest = matches[childRegex.SubexpIndex("rest")]
+			for range amt {
+				results[parent] = append(results[parent], child)
+			}
+		}
+	}
+	return results
+}
+
+var containsGold = map[string]bool{
+	"shiny gold": true,
+}
+
+func ContainsShinyGold(bag string, rules map[string]([]string)) bool {
+	hasGold, ok := containsGold[bag]
+	if ok {
+		return hasGold
+	}
+	children := utils.Unique(rules[bag])
+	for _, child := range children {
+		hasGold = ContainsShinyGold(child, rules)
+		containsGold[child] = hasGold
+		if hasGold {
+			break
+		}
+	}
+	containsGold[bag] = hasGold
+	return hasGold
+}
+
+func pt1(input string) (string, error) {
+	tree := ParseInput(input)
+	count := 0
+	for bag := range tree {
+		if bag != "shiny gold" && ContainsShinyGold(bag, tree) {
+			count++
+		}
+	}
+	fmt.Printf("count %v\n", count)
+	return strconv.Itoa(count), nil
+}
---
+++ solutions/2020/day7/pt2.go
@@ -0,0 +1,38 @@
+package day7
+
+import (
+	"advent-of-go/utils"
+	"fmt"
+	"strconv"
+)
+
+func Pt2() utils.Solution {
+	return utils.Solution{
+		Year:       2020,
+		Day:        7,
+		Part:       2,
+		Calculator: pt2,
+	}
+}
+
+func CountBags(bag string, rules map[string]([]string)) int {
+	children := rules[bag]
+	if len(children) == 0 {
+		return 1
+	}
+	result := 1
+	for _, child := range children {
+		result = result + CountBags(child, rules)
+	}
+	if bag == "shiny gold" {
+		result -= 1
+	}
+	return result
+}
+
+func pt2(input string) (string, error) {
+	tree := ParseInput(input)
+	result := CountBags("shiny gold", tree)
+	fmt.Printf("result %v\n", result)
+	return strconv.Itoa(result), nil
+}
